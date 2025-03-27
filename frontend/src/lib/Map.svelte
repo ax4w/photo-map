c0fe6e92a0faf21a8f633af9581e109d8c7f56f2
alex <mail@ax4w.me>
2025-03-27 23:00:19 +0100

    map

---
+++ frontend/src/lib/Map.svelte
@@ -0,0 +1,61 @@
+<script>
+	import { Map, TileLayer, Marker, Popup, DivIcon } from 'sveaflet';
+    import { fetchLocations } from '../api';
+    
+    let locationsPromise = $state(fetchLocations().then(locationsObj => {
+        return Object.entries(locationsObj).map(([region, location]) => ({
+            region,
+            lat: location.Lat,
+            lng: location.Long
+        }));
+    }));
+</script>
+
+<div id="body">
+	<Map
+		style="height: 100vh; width: 100%; background-color: #262626;"
+		options={{
+			center: [30, 0],
+			zoom: 5,
+			minZoom: 4,
+			maxZoom: 6,
+		}}>
+		<TileLayer 
+            url={"https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"} 
+            options = {{
+                 attribution: "&copy; <a href='https://carto.com/'>Carto</a>",
+                 noWrap : true
+            }}/>
+        {#await locationsPromise}
+            <div>Loading...</div>
+        {:then locations}
+            {#each locations as location}
+                <Marker latLng={[location.lat, location.lng]}>
+                    <DivIcon options={{
+                        className: 'emoji-pin',
+                        html: '📍', 
+                        iconSize: [30, 30],
+                        iconAnchor: [15, 30]
+                    }} />
+                </Marker>
+            {/each}
+        {/await}    
+	</Map>
+</div>
+
+<style>
+#body {
+    background-color: #262626;
+    margin: 0;
+    width: 100vw;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
+}
+
+</style>
